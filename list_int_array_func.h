27876797a8d5660ab2f2a4f7486797ae844e6255
Adrián Omar Riao Monsalve <47353281+AdrianRiao@users.noreply.github.com>
2020-02-11 11:24:50 +0100

    Añadido list_int_array_func.h

---
+++ list_int_array_func.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void init_List_IA(List_IA* p_list);
+
+//Añade elmento a la lista
+void add_List_IA(List_IA* p_list, IA* int_arr);
+
+void print_List_IA(List_IA list);
