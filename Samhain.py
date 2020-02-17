273fddab6736fe8778fafd18b5a25beb3fec7070
Adrián Omar Riao Monsalve <47353281+AdrianRiao@users.noreply.github.com>
2020-02-17 14:21:46 +0100

    Reducido al máximo brujas.py (2 últimos tests fallan)

--- Samhain.py
+++ Samhain.py
@@ -1,37 +1,18 @@
 import sys
 
-marmita = {}
-brujas_fail = []
-brujas_beben = 0
-
-def añadir(cosa):
-    try:
-        marmita[cosa] = marmita[cosa] + 1
-    except KeyError:
-        marmita[cosa] = 1
-
-def eliminar(cosa):
-    try:
-        n_cosas = marmita[cosa]
-    except KeyError:
-        return
-    
-    if n_cosas > 1:
-        marmita[cosa] = marmita[cosa] - 1
-    else:
-        del marmita[cosa]
+marmita = []
 
 num_brujas = int(sys.stdin.readline())
-
+string = ""
 for cont in range(num_brujas):
     letra, cosa = sys.stdin.readline().split()
     
     if (letra == 'a'):
-        añadir(cosa)
+        marmita.append(cosa)
     elif (letra == 'q'):
-        eliminar(cosa)
-    elif (letra == 'b'):
-        brujas_beben = brujas_beben + 1
         if cosa in marmita:
-            brujas_fail.append(cont)
-    cont = cont + 1
+            marmita.remove(cosa)
+    elif (letra == 'b'):
+        if cosa in marmita:
+            print(cont, end=' ')
+print()
